5bbfe64204033445ea284d95653540957c64f516
w3schools-test <raeebikash@gmail.com>
2025-01-06 12:37:03 +0545

    Learning Render HTML

--- DAY1_ReactJS/my-react-app/src/index.js
+++ DAY1_ReactJS/my-react-app/src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const myFirstElement = <h1>Hello React!</h1>
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myFirstElement);
+const myelement = (
+    <table>
+        <tr>
+            <th>Name</th>
+        </tr>
+        <tr>
+            <td>John</td>
+        </tr>
+        <tr>
+            <td>Elsa</td>
+        </tr>
+    </table>
+);
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(myelement);
 
 /*
 You are now watching
