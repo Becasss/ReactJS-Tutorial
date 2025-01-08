af17780e30db7df8afc1a2488a40b20fdfa79c61
w3schools-test <raeebikash@gmail.com>
2025-01-08 12:33:26 +0545

    Learning && Operator

--- DAY1_ReactJS/my-react-app/src/index.js
+++ DAY1_ReactJS/my-react-app/src/index.js
@@ -2,27 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 
-function MissedGoal() {
-    return <h1>Missed Goal</h1>
+function Garage(props) {
+    const cars = props.cars;
+    return (
+        <>
+            <h1>Garage</h1>
+            {
+                cars.length > 0 &&
+                <h2>You have {cars.length} cars in your Garage.</h2>
+            }
+        </>
+    );
 }
-
-function MadeGoal() {
-    return <h1>Made Goal</h1>
-}
-
-function Goal(props) {
-    const isGoal = props.isGoal;
-    if (isGoal) {
-        return <MadeGoal />;
-    } 
-    return <MissedGoal />;
-    
-}
-
+const cars = ["Ford", "Bmw", "Audi"];
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
-root.render(<Goal isGoal = {false} />);
+root.render(<Garage cars = {cars} />);
 
 /*
 You are now watching
