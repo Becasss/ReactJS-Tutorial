9ff76ffe3ca14b3ba8128089634eae389561e0ad
w3schools-test <raeebikash@gmail.com>
2025-01-16 16:39:55 +0545

    Update index.html

--- DAY1_ReactJS/my-react-app/src/index.js
+++ DAY1_ReactJS/my-react-app/src/index.js
@@ -1,24 +1,19 @@
+import '..module/.my-sass.scss';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-
-function Garage(props) {
-    const cars = props.cars;
+const Header = ( ) => {
     return (
         <>
-            <h1>Garage</h1>
-            {
-                cars.length > 0 &&
-                <h2>You have {cars.length} cars in your Garage.</h2>
-            }
+        <h1>Hello Style</h1>
+        <p>Add a little Style!</p>
         </>
-    );
+    )
 }
-const cars = ["Ford", "Bmw", "Audi"];
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
-root.render(<Garage cars = {cars} />);
+root.render(<Header/>);
 
 /*
 You are now watching
